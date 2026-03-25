9a3d945a6bebae8977e654f5f5fff342aa74f3cb
Jonathan Wilson <jfwfreo@tpgi.com.au>
2026-03-25 16:36:50 +1000

    Fix

--- MSVC_NMake/make.bat
+++ MSVC_NMake/make.bat
@@ -1,3 +1,3 @@
-set INCLUDE=%INCLUDE%;c:\temp\libxml2-v2.15.2\libxml2-install\include
+set INCLUDE=%INCLUDE%;c:\temp\libxml2-v2.15.2\install\include\libxml2
 nmake /f Makefile.vc CFG=release PERL=c:\cygwin64\bin\perl.exe
 nmake /f Makefile.vc CFG=debug PERL=c:\cygwin64\bin\perl.exe
